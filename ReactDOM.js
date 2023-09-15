370135bf4f21be31e3fc8bd5966e892fb0e32911
charanbhc <85993420+charanbhc@users.noreply.github.com>
2023-09-15 11:28:28 +0530

    Create ReactDOM.js

---
+++ ReactDOM.js
@@ -0,0 +1,49 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// ReactDOM.render(
+//   <div>
+//     <h1>Heading 1</h1>
+//     <p>Paragraph 1</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// var myElement = (
+//   <div>
+//     <h1>Heading 1</h1>
+//     <p>Paragraph 1</p>
+//   </div>
+// );
+
+// var myElement = (
+//   <div>
+//     <h1>Heading 1</h1>
+//     <p>5 + 2 = {5 + 2}</p>
+//   </div>
+// );
+
+// const myElement = (
+//     <ul>
+//       <li>Apples</li>
+//       <li>Bananas</li>
+//       <li>Cherries</li>
+//     </ul>
+//   );
+
+// let text = "Hello World!!!";
+// const myElement = (<h1>{text}</h1>);
+
+// const x = 1;
+// let text = "dummy";
+
+// if (x < 0) {
+//   text = "Good Morning";
+// } else {
+//   text = "Good Night";
+// }
+
+const myElement = <h1 className="myclass">Hello World</h1>;
+
+const myRoot = ReactDOM.createRoot(document.getElementById("root"));
+myRoot.render(myElement);
