344e3935e949d8dc291ab6ad6ac8e0acaa891192
Charan <bharicharanreddy@gmail.com>
2023-09-21 22:09:53 +0530

    Create React_components.js

---
+++ React_components.js
@@ -0,0 +1,40 @@
+import Heading from "./Heading";
+import MyList from "./MyList";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <MyList />
+    </div>
+  );
+}
+
+export default App;
+
+
+// Heading .js
+// import React from "react";
+
+// function Heading() {
+//   return <h1>My Favourite Apps -</h1>;
+// }
+
+// export default Heading;
+
+
+// MyList.js
+// import React from "react";
+
+// function MyList() {
+//   return (
+//     <ol>
+//       <li>Inshorts</li>
+//       <li>Groww</li>
+//       <li>Zerodha</li>
+//       <li>Spotify</li>
+//     </ol>
+//   );
+// }
+
+// export default MyList;
